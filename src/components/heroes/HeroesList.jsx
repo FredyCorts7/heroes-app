9cbf895bbf8e1a9ab5a3526f01e1cd02a1d5a6f5
fredycorts7 <fred.cor.14@gmail.com>
2020-09-15 15:52:52 -0500

    <ADD> Optimizando los componentes HeroesList y HeroScreen utilizando el hook useMemo

--- src/components/heroes/HeroesList.jsx
+++ src/components/heroes/HeroesList.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { getHeroesByPublisher } from '../../selectors/getHeroesByPublisher';
 import { HeroCard } from './HeroCard';
 
 export const HeroesList = ({ publisher }) => {
-  const heroes = getHeroesByPublisher(publisher);
+  const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
+  // const heroes = getHeroesByPublisher(publisher);
 
   return (
     <ul className='card-columns'>
