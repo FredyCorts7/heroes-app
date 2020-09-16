9e5155a83d1744e37d2c4992fb7b950969f03bfd
fredycorts7 <fred.cor.14@gmail.com>
2020-09-15 19:40:11 -0500

    <ADD> Implementando logout

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,9 +1,23 @@
 import React, { useContext } from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useHistory } from 'react-router-dom';
 import { AuthContext } from '../../auth/AuthContext';
+import { types } from '../../types/types';
 
 export const Navbar = () => {
-  const { user } = useContext(AuthContext);
+  const {
+    user: { name },
+    dispatch,
+  } = useContext(AuthContext);
+
+  const history = useHistory();
+
+  const handleLogOut = () => {
+    dispatch({
+      type: types.LOGOUT,
+    });
+
+    history.replace('/login');
+  };
 
   return (
     <nav className='navbar navbar-expand-sm navbar-dark bg-dark'>
@@ -44,15 +58,10 @@ export const Navbar = () => {
 
       <div className='navbar-collapse collapse w-100 order-3 dual-collapse2'>
         <ul className='navbar-nav ml-auto'>
-          <span className='nav-item nav-link text-info'>{user.name}</span>
-          <NavLink
-            activeClassName='active'
-            className='nav-item nav-link'
-            exact
-            to='/login'
-          >
+          <span className='nav-item nav-link text-info'>{name}</span>
+          <button className='nav-item nav-link btn' onClick={handleLogOut}>
             Logout
-          </NavLink>
+          </button>
         </ul>
       </div>
     </nav>
